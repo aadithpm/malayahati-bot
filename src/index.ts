0b438815e289e06a94c9fa1ca3215b3689bdb468
Aadith PM <aadithpm@gmail.com>
2023-10-14 17:04:40 -0500

    Add vars, verify passing vars from Workers config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import 'dotenv/config'
+
+export interface EnvVar {
+  // stored in Workers env vars
+  DISCORD_TOKEN: string
+  DISCORD_APP_ID: string
+  DISCORD_PUBLIC_KEY: string
+}
+
 export default {
-  async fetch (request: Request) {
-    return new Response(`request method: ${request.method}`)
+  async fetch (request: Request, env: EnvVar) {
+    return new Response(`request method: ${request.method} ${env.DISCORD_APP_ID}`)
   }
 }
