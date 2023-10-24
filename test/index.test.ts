146daf428471b2983e0748f7700330269e76b854
Aadith PM <aadithpm@gmail.com>
2023-10-23 23:42:49 -0500

    Clean up tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,11 @@
-import { expect } from "chai";
-import { describe, it, beforeEach, afterEach } from "mocha";
-import {
-  InteractionResponseType,
-  InteractionType,
-  InteractionResponseFlags,
-} from "discord-interactions";
-import sinon from "sinon";
-import { Env, default as bot } from "../src/index";
+import { expect, test } from "vitest";
 
 const TEST_ENV_VAR = {
   DISCORD_TOKEN: "1234",
   DISCORD_APP_ID: "1a2b3c",
   DISCORD_PUBLIC_KEY: "abcd",
 };
+
+test("canary test", () => {
+  expect(true).toBe(true);
+});
